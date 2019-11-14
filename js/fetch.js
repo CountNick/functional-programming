fab049556669d1934653e462b1814e98cc5bf15d
CountNick <meijer.nick1@gmail.com>
2019-11-14 21:41:44 +0100

    :recycle:Refactored some code

--- js/fetch.js
+++ js/fetch.js
@@ -1,79 +1,4 @@
-
-
-const svg = d3.select('svg');
-
-const width = +svg.attr('width');
-const height = +svg.attr('height');
-
-const render = objectsArray => {
-  
-  const xValue = row => row.amount;
-  const yValue = originRow => originRow.origin;
-  const margin = { top: 40, right: 30, bottom: 70, left: 120 };
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.top - margin.bottom;
-  
-  const xScale = d3.scaleLinear()
-  	.domain([0, d3.max(objectsArray, xValue)])
-  	.range([0, innerWidth])
-  	.nice();
-  
-  const yScale = d3.scalePoint()
-  	.domain(objectsArray.map(yValue))
-  	.range([0, innerHeight])
-  	.padding(0.7)
-  
-  //check to see if range is right
-  console.log(xScale.range())
-  
-  const g = svg.append('g')
-  	.attr('transform', `translate(${margin.left}, ${margin.top})`);
-  
-  g.append('g')
-    .call(d3.axisLeft(yScale)
-         		.tickSize(-innerWidth))
-  	.append('text')
-  	.attr('fill', 'black')
-  	//.attr('y', innerHeight /2)
-  	//.text('Continenten');
-  
-  g.append('g')
-    .call(d3.axisBottom(xScale)
-         .tickSize(-innerHeight))
-  	.attr('transform', `translate(0, ${innerHeight})`)
-    
- 		.append('text')
-  	.attr('y', 60)
-  	.attr('x', innerWidth / 2)
-  	.attr('fill', 'black')
-  	.text('Aantal pijpen')
- 
-  const color = d3.scaleOrdinal()
-    .domain(["hasjpijpen", "tabakspijpen", "waterpijpen", "pijpen (rookgerei)", "opiumpijpen" ])
-    .range([ "#FF0047", "#FF8600", "#6663D5", "#FFF800"])
-  
-	g.selectAll('circle')
-    .data(objectsArray)
-  	.enter()
-    	.append('circle')
-  			.attr('cy', d => yScale(yValue(d)))
-  			.attr('cx', d => xScale(xValue(d)))
-  			.attr('r', 15)
-  			.style("fill", d => { return color(d.type) } )
-
-  g.append('text')
-  	.attr('y', -10)
-  	.text('Aantal rookgerei naar soort, per continent')
-  
-  
-}
-
-function generateColor(object){
-
-console.log(object)
-
-
-}
+console.log(d3.symbol())
 
 //puts the endpoint in a variable
 const url ="https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-36/sparql"
@@ -107,24 +32,104 @@ WHERE {
 } #GROUP BY ?herkomstSuper ?herkomstSuperLabel 
 `
 
-
+// fetch data from endpoint
 d3.json(url+"?query="+ encodeURIComponent(query) +"&format=json")
     .then(data => data)
     .then(json => {
         
+        // pur fetch results in variable
         let data = json.results.bindings
 
+        //map over data objects and make new array filled with modified objects
         const objectsArray = data
             .map(object => {
                 
-                object.amount = +object.amount.value
+                object.amount = parseInt(object.amount.value)
                 object.type = object.typeLabel.value
                 object.origin = object.herkomstSuperLabel.value
 
-                console.log(object)
                 return object
-            })//.filter(type => {if (type.type == "pijpen (rookgerei)") return type})
-        
-        render(objectsArray)
-
+            })
+        //invoke the renderGraph function and pass the new array with it
+        renderGraph(objectsArray)
     })
+
+
+
+
+function renderGraph(objectsArray) {
+
+    //select the svg element in index.html
+    const svg = d3.select('svg');
+    //sets height and width to height and width of svg element
+    const width = +svg.attr('width');
+    const height = +svg.attr('height');
+    //sets x and y values to the values of amount and origin
+    const xValue = d => d.amount;
+    const yValue = d => d.origin;
+    const margin = { top: 40, right: 30, bottom: 70, left: 120 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+  
+    const xScale = d3.scaleLinear()
+        .domain([0, d3.max(objectsArray, xValue)])
+        .range([0, innerWidth])
+        .nice();
+  
+    const yScale = d3.scalePoint()
+        .domain(objectsArray.map(yValue))
+        .range([0, innerHeight])
+        .padding(0.7);
+  
+  
+    const g = svg.append('g')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`);
+  
+    g.append('g')
+        .call(d3.axisLeft(yScale)
+            .tickSize(-innerWidth))
+  	    .append('text')
+  	    .attr('fill', 'black');
+  	    //.attr('y', innerHeight /2)
+  	    //.text('Continenten');
+  
+    g.append('g')
+        .call(d3.axisBottom(xScale)
+            .tickSize(-innerHeight))
+  	    .attr('transform', `translate(0, ${innerHeight})`)
+        .append('text')
+  	    .attr('y', 60)
+  	    .attr('x', innerWidth / 2)
+  	    .attr('fill', 'black')
+  	    .text('Aantal pijpen');
+
+    //makes an ordinal color scale for each type
+    const color = d3.scaleOrdinal()
+        .domain(["hasjpijpen", "tabakspijpen", "waterpijpen", "pijpen (rookgerei)", "opiumpijpen" ])
+        .range([ "#FF0047", "#FF8600", "#6663D5", "#FFF800"]);
+  
+    //draw the circles in the graph
+	g.selectAll('circle')
+        .data(objectsArray)
+  	    .enter()
+        .append('circle')
+            .transition()
+  			.attr('cy', d => yScale(yValue(d)))
+  			.attr('cx', d => xScale(xValue(d)))
+  			.attr('r', 15)
+            .style('fill', d => { return color(d.type) } );
+              
+    g.selectAll('rect')
+        .data(objectsArray)
+        .enter()
+        .append('rect')
+            .attr('height', 20)
+            .attr('width', 20)
+            .attr('x', innerWidth)
+            .style('fill', d => { return color(d.type)})
+
+    //sets the graph title
+    g.append('text')
+        .attr('y', -10)
+  	    .text('Aantal rookgerei naar type en continent')
+}
